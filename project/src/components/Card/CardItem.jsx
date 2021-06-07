13fdef86d56d87d61ca1cd0b4b4bfee3214d4104
anastasia <smolina.anastasia.14@gmail.com>
2021-06-07 21:10:09 +0300

    feat: add CardItem component with state

---
+++ project/src/components/Card/CardItem.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import WordInfo from './WordInfo';
+import ShowWord from './ShowWord';
+
+export default class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pressed: false
+        };
+    }
+
+    handleChange = () => {
+        this.setState(
+            {
+                pressed: !this.state.pressed
+            }
+        );
+    }
+    render() {
+        const {transcription, russian, tags, english, id} = this.props;
+        return(
+            <div className="card">
+                <div className="card-word">{english}</div>
+                {this.state.pressed ? (
+                    <WordInfo transcription={transcription} russian={russian} tags={tags} id={id}></WordInfo>
+                ) : (
+                    <ShowWord show={this.handleChange}></ShowWord>
+                )}
+            </div>
+        );
+}}
--- project/src/components/CardList/index.jsx
+++ project/src/components/CardList/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Card from '../Card/index';
+import CardItem from '../Card/CardItem';
 import words from '../../data';
 
 export default function CardList(props) {
     return(
         <section className="card-box">
             {
-            words.map((word) => <Card id={word.id} english={word.english} transcription={word.transcription} russian={word.russian} tags={word.tags} isSelected={word.isSelected}></Card>)
+            words.map((word) => <CardItem id={word.id} english={word.english} transcription={word.transcription} russian={word.russian} tags={word.tags} isSelected={word.isSelected}></CardItem>)
             }
         </section>
     );
