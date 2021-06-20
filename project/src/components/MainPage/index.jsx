f42a92523f09bde7288e499fe2dd24fc368c0445
anastasia <smolina.anastasia.14@gmail.com>
2021-06-20 19:37:14 +0300

    feat: add fetch

--- project/src/components/MainPage/index.jsx
+++ project/src/components/MainPage/index.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import WordList from '../WordList/index';
-import words from '../../data';
+// import words from '../../data';
 
 export default class MainPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            words: [],
+            isLoading: false,
+            error:null,
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true });
+
+        fetch('/api/words')
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong ...');
+                }
+            })
+            .then((response) => {
+                this.setState({
+                    words: response,
+                    isLoading: false,
+                })
+            })
+            .catch(error => this.setState({ error, isLoading: false }));
+    }
     render() {
+        const { words, isLoading, error } = this.state;
+
+        if (error) {
+            return <p>{error.message}</p>;
+        }
+
+        if (isLoading) {
+            return <p>Loading ...</p>;
+        }
         return(
         <div className="words-list">
             <h2 className="words-list_item">Все слова</h2>
